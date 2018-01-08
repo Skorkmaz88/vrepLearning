f6eb8414478f2c5c31356670ce2d3466ce651e14
semih <skorkmaz88@yahoo.com>
2018-01-08 18:29:58 +0300

    Arm movements

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ vrep.simxFinish(-1) # Close it all
 clientID=vrep.simxStart('127.0.0.1',19997,True,True,5000,5) # Connect to V-REP
 
 if clientID!=-1:
-    print ('Connected to remote API server')
-
     # enable the synchronous mode on the client:
     vrep.simxSynchronous(clientID,True)
-
     # start the simulation:
     vrep.simxStartSimulation(clientID,vrep.simx_opmode_blocking)
+    # Connect to kinect sensor
+
+    # Connect to robotic arm
+    _, handle = vrep.simxGetObjectHandle(clientID, 'UR5_joint1', vrep.simx_opmode_oneshot)
+    _, handle2 = vrep.simxGetObjectHandle(clientID, 'UR5_joint2', vrep.simx_opmode_oneshot)
+    _, handle3 = vrep.simxGetObjectHandle(clientID, 'UR5_joint3', vrep.simx_opmode_oneshot)
+    _, handle4 = vrep.simxGetObjectHandle(clientID, 'UR5_joint4', vrep.simx_opmode_oneshot)
 
     # Now step a few times:
     for i in range(1,10):
