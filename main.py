1944bc9c9ddc6cb33e187a47c79f80fcde959fdd
semih <skorkmaz88@yahoo.com>
2018-01-15 16:41:21 +0300

    should be final

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ import matplotlib.pyplot as plt
 from skimage import io, exposure, img_as_uint, img_as_float
 import pickle
 import time
+import math
 
 
 # Sample
-def sampleAngles(n_samples = 10000, num_of_joints = 4, mu = 0.0, sigma = 0.3):
+def sampleAngles(n_samples = 10000, num_of_joints = 4, mu = 0.0, sigma = 15.0):
     angle_list = []
     for i in range(n_samples):
         angle_list.append(np.random.normal(mu, sigma, num_of_joints))
@@ -24,11 +25,11 @@ n_samples = 100
 # Create n number of frames, for sequence for one angle
 interpolation_steps = 4
 # Create samples ?
-#angle_list = sampleAngles()
+angle_list = sampleAngles()
 
 # or read pickle ?
-with open('angles.pkl', 'r') as f:
-    angle_list = pickle.load(f)
+#with open('angles.pkl', 'r') as f:
+#    angle_list = pickle.load(f)
 
 print "Running the script.."
 vrep.simxFinish(-1) # Close it all
@@ -63,7 +64,10 @@ if clientID!=-1:
         for j in range(4):
             if i == 0:
                 print 'Skipping the first step'
-                continue
+                err, resolution, image = vrep.simxGetVisionSensorImage(clientID,kinectDepth, 0, vrep.simx_opmode_streaming)
+
+                vrep.simxSynchronousTrigger(clientID);
+                break
             else:
                 joint_angles = angle_list[i - 1]
             # Interpolate here
@@ -73,10 +77,10 @@ if clientID!=-1:
             angle_3 = (joint_angles[3] /  (interpolation_steps)  ) * (j + 1)
 
 
-            vrep.simxSetJointTargetPosition(clientID, handle, angle_0,  vrep.simx_opmode_oneshot )
-            vrep.simxSetJointTargetPosition(clientID, handle2, angle_1,  vrep.simx_opmode_oneshot)
-            vrep.simxSetJointTargetPosition(clientID, handle3, angle_2,  vrep.simx_opmode_oneshot )
-            vrep.simxSetJointTargetPosition(clientID, handle4, angle_3,  vrep.simx_opmode_oneshot )
+            vrep.simxSetJointTargetPosition(clientID, handle, math.radians(angle_0),  vrep.simx_opmode_oneshot )
+            vrep.simxSetJointTargetPosition(clientID, handle2, math.radians(angle_1),  vrep.simx_opmode_oneshot)
+            vrep.simxSetJointTargetPosition(clientID, handle3, math.radians(angle_2),  vrep.simx_opmode_oneshot )
+            vrep.simxSetJointTargetPosition(clientID, handle4, math.radians(angle_3),  vrep.simx_opmode_oneshot )
             # Wait for the motion
             time.sleep(2)
             err, resolution, image = vrep.simxGetVisionSensorImage(clientID,kinectDepth, 0, vrep.simx_opmode_streaming)
@@ -86,7 +90,7 @@ if clientID!=-1:
                 im.resize(480,640,3)
 
 
-                io.imsave('depth_' + str(i) +'_' + str(j)+ ' .png', im)
+                io.imsave('depth_' + str(i-1) +'_' + str(j)+ ' .png', im)
 
 
             elif err == vrep.simx_return_novalue_flag:
